691922e9e7889652333f547336ae57ea632e8559
Enes Şahin <enes_sahin_tr@hotmail.com>
2023-05-22 14:36:42 +0300

    Update

--- admin/dashboardv1/flutter_service_worker.js
+++ admin/dashboardv1/flutter_service_worker.js
@@ -44,8 +44,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "1ac7fa110525dc6a288051582aa068b2",
-"/": "1ac7fa110525dc6a288051582aa068b2",
+"index.html": "12bf167e169b881c6d94bb8c682c476a",
+"/": "12bf167e169b881c6d94bb8c682c476a",
 "main.dart.js": "f8bc36bc1428e2e4ba253e7c86d05296",
 "manifest.json": "d003b82b9a8dbbd6f09729891fd922e2",
 "version.json": "3e83f6587ce888c154861812e44eb323"
